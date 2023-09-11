bde4ae88f051f440ed113a3ce162bc1b69418ffa
SKILLETTON2K <ashes2426@gmail.com>
2023-09-10 19:21:21 -0700

    Added HOME pages

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import '../App/App.css'
+import HeroSection from '../../components/HeroSection/HeroSection'
+import Cards from '../../components/Cards/Cards';
+import Footer from '../../components/Footer/Footer';
+
+function Home () {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+            <Footer />
+        </>
+    )
+}
+
+export default Home;
