b1e1c348adaed806cb1583a5e0f88e91221057ec
SKILLETTON2K <ashes2426@gmail.com>
2023-09-10 19:17:23 -0700

    Added in my Card Items for my Home Page

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from '../CardItem/CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these amazing builds!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-9.jpg'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Featured'
+              path='/schematics'
+            />
+            <CardItem
+              src='images/img-3.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Featured'
+              path='/schematics'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-2.jpg'
+              text='Signed in? Check out our entire schematic library!'
+              label='Schematics'
+              path='/schematics'
+            />
+            <CardItem
+              src='images/img-1.jpg'
+              text='Check out our FAQ page that may answer your questions!'
+              label='FAQs'
+              path='/faqs'
+            />
+            <CardItem
+              src='images/img-8.jpg'
+              text='Sign up today get access to our entire library!'
+              label='Sign-Up'
+              path='/sign-up'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,54 +1,54 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-  export default function PhotoUpload() {
-    const [selectedFile, setSelectedFile] = useState(null);
+//   export default function PhotoUpload() {
+//     const [selectedFile, setSelectedFile] = useState(null);
   
-    const handleFileChange = (event) => {
-      const file = event.target.files[0];
-      setSelectedFile(file);
-    };
+//     const handleFileChange = (event) => {
+//       const file = event.target.files[0];
+//       setSelectedFile(file);
+//     };
   
-    return (
-    <>
+//     return (
+//     <>
 
-      <form id="new-form" action="/schematic" method="POST">
-        <h2>Create a Schematic Post</h2>
+//       <form id="new-form" action="/schematic" method="POST">
+//         <h2>Create a Schematic Post</h2>
 
-        <label>* Name of the build:</label>
-    <input type="text" name="title"></input>
-    <br></br>
-    <br></br>
+//         <label>* Name of the build:</label>
+//     <input type="text" name="title"></input>
+//     <br></br>
+//     <br></br>
 
-    <label>* Description:</label>
-    <input type="text" name="description"></input>
-    <br></br>
-    <br></br>
+//     <label>* Description:</label>
+//     <input type="text" name="description"></input>
+//     <br></br>
+//     <br></br>
 
-    <label>Amount of blocks:</label>
-    <input type="number" name="blocks"></input>
-    <br></br>
-    <br></br>
+//     <label>Amount of blocks:</label>
+//     <input type="number" name="blocks"></input>
+//     <br></br>
+//     <br></br>
 
-    <label>Preview Photo:</label>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleFileChange}
-        />
-        {selectedFile && (
-           <div>
-            <h3>Build Preview:</h3>
-            <img
-              src={URL.createObjectURL(selectedFile)}
-              alt="Selected"
-              width="300"
-              height="300"
-            />
-          </div>
-        )}
+//     <label>Preview Photo:</label>
+//         <input
+//           type="file"
+//           accept="image/*"
+//           onChange={handleFileChange}
+//         />
+//         {selectedFile && (
+//            <div>
+//             <h3>Build Preview:</h3>
+//             <img
+//               src={URL.createObjectURL(selectedFile)}
+//               alt="Selected"
+//               width="300"
+//               height="300"
+//             />
+//           </div>
+//         )}
 
-          <input class="addButton" type="submit" value="Post Schematic"></input>
-      </form>
-    </>
-    );
-  }
+//           <input class="addButton" type="submit" value="Post Schematic"></input>
+//       </form>
+//     </>
+//     );
+//   }
