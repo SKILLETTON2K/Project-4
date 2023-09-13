1331382b72f6720e981904f3d76261fe7a2f3950
SKILLETTON2K <ashes2426@gmail.com>
2023-09-12 18:41:14 -0700

    Updated README file

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-Testing
+== == == == == == == == == == == == == == ==
+
+<h1>Schematic Static</h1>
+
+== == == == == == == == == == == == == == ==
+
+<h5>Sample Photo's:<h5>
+
+
+
+
+
+== == == == == == == == == == == == == == ==
+
+<h5>In this project we used:<h5>
+<li>
+Node.js
+React.js
+Express
+MongoDB
+Mongoose
+FontAwesome
+CSS
+HTML
+</li>
+
+== == == == == == == == == == == == == == ==
+
+<h5>Heroku & Trello Board Links:<h5>
+<li>
+https://schematic-static-3311e5bb67ef.herokuapp.com/
+== == ==
+https://trello.com/b/fUis7hys/project-4
+</li>
+
+== == == == == == == == == == == == == == ==
+
+<h5>Future Plans for Schematic Static:</h5>
+<li>
+Add in a filter/search sytem for the shematic library once it's starts to fill up
+Add in a 'like' system to allow users to rate others posts
+Add in an email newsletter for any updates to the site
+Swap over to Google sign in system
+Add in drop down answers to the FAQ page.
+</li>
+
+== == == == == == == == == == == == == == ==
+
--- src/components/Schematics/Schemcards.jsx
+++ src/components/Schematics/Schemcards.jsx
@@ -5,48 +5,55 @@ import CardItem from '../CardItem/CardItem'
 
 
 function Schemcards() {
-    const fileUrl = '/path/to/your/downloadable/file.pdf';
+    const fileUrl = '/public/schems/first-home.schem';
+
   return (
     <div className='schemcards'>
       <div className='schemcards__container'>
         <div className='schemcards__wrapper'>
           <ul className='schemcards__items'>
+
             <CardItem
-             src='images/img-9.jpg'
-             text='G.A. Logo &nbsp; || &nbsp; Blocks: 1,674 &nbsp; || &nbsp; By: Ashton'
-             label='Featured Download'
-             path='/schematics'
-        >
-            <a href={fileUrl} download="first-home.schem">
-            Download File
-            </a>
-            </CardItem>
+              src='images/img-9.jpg'
+              text='G.A. Logo &nbsp; || &nbsp; Blocks: 1,674 &nbsp; || &nbsp; By: Ashton &nbsp;'
+              label='Download PDF File'
+              path='/schematics'
+              fileUrl={fileUrl}
+              fileName='first-home.schem'
+            />
+
             <CardItem
               src='images/img-3.jpg'
               text='3 Story House &nbsp; || &nbsp; Blocks: 25,172 &nbsp; || &nbsp; By: Ashton'
               label ='Featured Download'
               path='/schematics'
             />
+  
           </ul>
+
           <ul className='schemcards__items'>
+
             <CardItem
               src='images/img-4.jpg'
               text='Amethyst Home &nbsp; || &nbsp;Blocks: 5,120 &nbsp; || &nbsp; By: Marina'
               label='Download'
               path='/schematics'
             />
+
             <CardItem
               src='images/img-5.jpg'
               text='Sugarcane Farm &nbsp; || &nbsp;Blocks: 1,540 &nbsp; || &nbsp; By: Marina'
               label='Download'
               path='/faqs'
             />
+
             <CardItem
               src='images/img-6.jpg'
               text='Mushroom Home &nbsp;||&nbsp;Blocks:14,300&nbsp; || &nbsp; By: Anna'
               label='Download'
               path='/'
             />
+
             <CardItem
               src='images/img-7.jpg'
               text='Windmill Build  &nbsp;  ||  &nbsp;Blocks: 11,067 &nbsp;|| &nbsp;   By: Anna'
@@ -60,6 +67,7 @@ function Schemcards() {
               label='Download'
               path='/faqs'
             />
+
           </ul>
         </div>
       </div>
